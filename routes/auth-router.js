4075d3eaba83558c6d6c7b9bb0d5a1dd0e5e46a8
Reginald Léandre <leandrereginald@gmail.com>
2023-10-27 19:58:10 -0400

    dummy code for auth

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+router.post("/register", async (req, res, next) => {});
 
+
+router.post("/login", async (req, res, next) => {});
 
 module.exports = router;
